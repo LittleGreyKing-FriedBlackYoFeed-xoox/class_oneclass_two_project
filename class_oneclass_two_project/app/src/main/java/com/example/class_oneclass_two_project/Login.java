ecf4f1c5c5adcb8bdf03d90ad5909eafcae0d211
unknown <704382210@qq.com>
2019-05-21 12:35:32 +0800

    完成登录界面

---
+++ class_oneclass_two_project/app/src/main/java/com/example/class_oneclass_two_project/Login.java
@@ -0,0 +1,17 @@
+package com.example.class_oneclass_two_project;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        //透明状态栏          
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+    }
+}
