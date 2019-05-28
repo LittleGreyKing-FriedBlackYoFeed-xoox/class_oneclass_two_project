3d1666ad245aacfb897031c1a6cfba8dd656e61d
unknown <704382210@qq.com>
2019-05-28 12:19:37 +0800

    添加了图片素材

--- class_oneclass_two_project/app/src/main/java/com/example/class_oneclass_two_project/Login.java
+++ class_oneclass_two_project/app/src/main/java/com/example/class_oneclass_two_project/Login.java
@@ -11,8 +11,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class Login extends AppCompatActivity implements View.OnClickListener{
+public class Login extends AppCompatActivity implements View.OnClickListener {
     private MyDatabaseHelper dbHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,55 +22,60 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         //实例化数据库
-        dbHelper = new MyDatabaseHelper(this,"User.db",null,2);
+        dbHelper = new MyDatabaseHelper(this, "User.db", null, 2);
         //创建数据库成功且执行了创建user表操作
         dbHelper.getWritableDatabase();
         //Toast.makeText(Login.this,"create database successed!",Toast.LENGTH_LONG).show();
         //监听按钮事件
-        Button login = (Button)findViewById(R.id.login);
+        Button login = (Button) findViewById(R.id.login);
         login.setOnClickListener(this);
         //注册
-        Button r = (Button)findViewById(R.id.Sign_in);
+        Button r = (Button) findViewById(R.id.Sign_in);
         r.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             //注册跳转
             case R.id.Sign_in:
-                Intent i = new Intent(Login.this,Register.class);
+                Intent i = new Intent(Login.this, Register.class);
                 startActivity(i);
                 break;
             case R.id.login:
                 //1、获取页面信息
-                EditText editText1 = (EditText)findViewById(R.id.username);
+                EditText editText1 = (EditText) findViewById(R.id.username);
                 String username = editText1.getText().toString();
-                EditText editText2 = (EditText)findViewById(R.id.password);
+                EditText editText2 = (EditText) findViewById(R.id.password);
                 String password = editText2.getText().toString();
-                Toast.makeText(Login.this,username + password,Toast.LENGTH_SHORT).show();
-                //2、查询数据库存储的信息
-                SQLiteDatabase db = dbHelper.getWritableDatabase();
-                Cursor c = db.query("user_base",null,null,null,null,null,null);
-                if (c.moveToFirst()){
-                    do {
-                        String usernamefordatabases = c.getString(c.getColumnIndex("username"));
-                        String passwordfordatabases = c.getString(c.getColumnIndex("password"));
-                        //Toast.makeText(Login.this,usernamefordatabases+passwordfordatabases,Toast.LENGTH_SHORT).show();
-                        //3、用户填写的用户名和密码与数据库用户名密码进行比较
-                        if (username.equals(usernamefordatabases)){
-                            if (password.equals(passwordfordatabases)){
-                                //4、跳转到书目列表
-                                Intent b = new Intent(Login.this,Book.class);
-                                startActivity(b);
-                                //Toast.makeText(Login.this,"Login successed!",Toast.LENGTH_SHORT).show();
-                            }else{
-                                Toast.makeText(Login.this,"password error,please check try again latter!",Toast.LENGTH_SHORT).show();
+                //Toast.makeText(Login.this,username + password,Toast.LENGTH_SHORT).show();
+                if (!username.equals("") && !password.equals("")) {
+                    //Toast.makeText(Login.this, "不為空", Toast.LENGTH_SHORT).show();
+                    //2、查询数据库存储的信息
+                    SQLiteDatabase db = dbHelper.getWritableDatabase();
+                    Cursor c = db.query("user_base", null, null, null, null, null, null);
+                    if (c.moveToFirst()) {
+                        do {
+                            String usernamefordatabases = c.getString(c.getColumnIndex("username"));
+                            String passwordfordatabases = c.getString(c.getColumnIndex("password"));
+                            //Toast.makeText(Login.this,usernamefordatabases+passwordfordatabases,Toast.LENGTH_SHORT).show();
+                            //3、用户填写的用户名和密码与数据库用户名密码进行比较
+                            if (username.equals(usernamefordatabases)) {
+                                if (password.equals(passwordfordatabases)) {
+                                    //4、跳转到书目列表
+                                    Intent b = new Intent(Login.this, Book.class);
+                                    startActivity(b);
+                                    //Toast.makeText(Login.this,"Login successed!",Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(Login.this, "password error,please check try again latter!", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(Login.this, "username error,please check and try again latter!", Toast.LENGTH_SHORT).show();
                             }
-                        }else{
-                            Toast.makeText(Login.this,"username error,please check and try again latter!",Toast.LENGTH_SHORT).show();
-                        }
-                    }while (c.moveToNext());
+                        } while (c.moveToNext());
+                    }
+                } else {
+                    Toast.makeText(Login.this, "username or password can't empty!", Toast.LENGTH_SHORT).show();
                 }
                 break;
         }
